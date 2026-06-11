954a21d1b76acb7d14297e18bc96a8e4ecec6e7d
Brendan Callender <bcall@ssreng.com>
2026-06-11 15:56:10 -0700

    remove virtual memory header and just include .c

--- base_vmemory.c
+++ base_vmemory.c
@@ -1,5 +1,18 @@
 
-#include "base_vmemory.h"
+#include "base_core.h"
+
+typedef struct
+{
+    uint64_t page_size;
+    uint64_t allocation_granularity;
+
+} VirtualMemoryInfo;
+
+static VirtualMemoryInfo vm_get_info   (void);
+static void*             vm_reserve    (uint64_t size);
+static bool32            vm_commit     (void* ptr, uint64_t size);
+static bool32            vm_decommit   (void* ptr, uint64_t size);
+static void              vm_release    (void* ptr, uint64_t size);
 
 /*********************************************************************************/
 #if defined(PLATFORM_WINDOWS)
