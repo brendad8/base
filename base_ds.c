32377a75e54ef975805906165867812e25bfab34
Brendan Callender <bcall@ssreng.com>
2026-06-18 13:03:00 -0700

    finish migration to stb_ds dynamic array + hashmap

--- base_ds.c
+++ base_ds.c
@@ -1,143 +1,141 @@
 
-#include <stdlib.h>
-
-#include "base_ds.h"
-#include "base_arena.h"
+// #include "base_ds.h"
+// #include "base_arena.h"
 
 #define STBDS_NO_SHORT_NAMES
 #define STBDS_IMPLEMENTATION
 #include "deps/stb_ds.h"
 
-/*********************************************************************************/
-void* array_grow_arena(Arena* arena, void* items, uint64_t item_size, uint64_t count)
-{
-    void* new_ptr;
-    uint64_t new_len;
-    uint64_t new_cap;
-
-    uint64_t len = ARRAY_LEN(items);
-    uint64_t cap = ARRAY_CAP(items);
-
-    new_len = len + count;
-    if (new_len < cap)
-        return items;
-
-    if (new_len < 2 * cap)
-        new_cap = 2 * cap;
-    else if (new_len < 4)
-        new_cap = 4; // minimum capacity of 4
-    else // NOTE(bcall): if new_len > 2 * capacity, then set cap to 1.5*new_len
-        new_cap = (uint64_t)(3 * new_len / 2);
-
-    if (items == NULL)
-    {
-        void* ptr = arena_push(arena, sizeof(ArrayHeader) + new_cap * item_size);
-        if (ptr)
-        {
-            ArrayHeader* hdr = (ArrayHeader*)ptr;
-            hdr->cap = new_cap;
-            hdr->len = 0;
-            return (void*)(hdr + 1);
-        }
-        else // WARN(bcall): memory allocation fails hiddenly... 
-        {
-            return items; 
-        }
-    }
-    // NOTE(bcall): Array has room to grow in arena without relocating it...
-    else if ((uint64_t)(arena->base + arena->pos) == (uint64_t)items + cap*item_size)
-    {
-        // NOTE(bcall): since array is not moving we allocate space for diff in new vs old capacity
-        void* ptr = arena_push(arena, (new_cap - cap) * item_size);
-        if (ptr)
-        {
-            ARRAY_HEADER_CAST(items)->cap = new_cap;
-            return items;
-        }
-        else // WARN(bcall): memory allocation fails hiddenly... 
-        {
-            return items; 
-        }
-    }
-    else
-    {
-        // NOTE(bcall): push room for entire new cap and move to new location...
-        void* ptr = arena_push(arena, sizeof(ArrayHeader) + new_cap * item_size);
-        if (ptr)
-        {
-            ArrayHeader* hdr = (ArrayHeader*)ptr;
-            hdr->cap = new_cap;
-
-            ptr = (void*)(hdr + 1);
-            memmove(ptr, items, len * item_size);
-            
-            // TODO(bcall): free prev items pointer if arena has free list...
-            return ptr;
-
-        }
-        else // WARN(bcall): memory allocation fails hiddenly... 
-        {
-            return items;
-        }
-    }
-}
-
-/*********************************************************************************/
-void* array_grow_heap(void* items, uint64_t item_size, uint64_t count)
-{
-    void* new_ptr;
-    uint64_t new_len;
-    uint64_t new_cap;
-
-    uint64_t len = ARRAY_LEN(items);
-    uint64_t cap = ARRAY_CAP(items);
-
-    new_len = len + count;
-    if (new_len < cap)
-        return items;
-
-    if (new_len < 2 * cap)
-        new_cap = 2 * cap;
-    else if (new_len < 4)
-        new_cap = 4;
-    else
-        new_cap = (uint64_t)(3 * new_len / 2);
-
-    if (items == NULL)
-    {
-        void* ptr = malloc(sizeof(ArrayHeader) + new_cap * item_size);
-        if (ptr)
-        {
-            ArrayHeader* hdr = (ArrayHeader*)ptr;
-            hdr->cap = new_cap;
-            hdr->len = 0;
-            return (void*)(hdr + 1);
-        }
-        else // WARN(bcall): memory allocation fails hiddenly... 
-        {
-            return items; 
-        }
-    }
-    else
-    {
-        void* ptr = realloc(ARRAY_HEADER_CAST(items), sizeof(ArrayHeader) + new_cap * item_size);
-        if (ptr)
-        {
-            ArrayHeader* hdr = (ArrayHeader*)ptr;
-            hdr->cap = new_cap;
-            hdr->len = len;
-
-            ptr = (void*)(hdr + 1);
-            memmove(ptr, items, len * item_size);
-            
-            return ptr;
-
-        }
-        else // WARN(bcall): memory allocation fails hiddenly... 
-        {
-            return items;
-        }
-    }
-}
+// /*********************************************************************************/
+// void* array_grow_arena(Arena* arena, void* items, uint64_t item_size, uint64_t count)
+// {
+//     void* new_ptr;
+//     uint64_t new_len;
+//     uint64_t new_cap;
+//
+//     uint64_t len = ARRAY_LEN(items);
+//     uint64_t cap = ARRAY_CAP(items);
+//
+//     new_len = len + count;
+//     if (new_len < cap)
+//         return items;
+//
+//     if (new_len < 2 * cap)
+//         new_cap = 2 * cap;
+//     else if (new_len < 4)
+//         new_cap = 4; // minimum capacity of 4
+//     else // NOTE(bcall): if new_len > 2 * capacity, then set cap to 1.5*new_len
+//         new_cap = (uint64_t)(3 * new_len / 2);
+//
+//     if (items == NULL)
+//     {
+//         void* ptr = arena_push(arena, sizeof(ArrayHeader) + new_cap * item_size);
+//         if (ptr)
+//         {
+//             ArrayHeader* hdr = (ArrayHeader*)ptr;
+//             hdr->cap = new_cap;
+//             hdr->len = 0;
+//             return (void*)(hdr + 1);
+//         }
+//         else // WARN(bcall): memory allocation fails hiddenly... 
+//         {
+//             return items; 
+//         }
+//     }
+//     // NOTE(bcall): Array has room to grow in arena without relocating it...
+//     else if ((uint64_t)(arena->base + arena->pos) == (uint64_t)items + cap*item_size)
+//     {
+//         // NOTE(bcall): since array is not moving we allocate space for diff in new vs old capacity
+//         void* ptr = arena_push(arena, (new_cap - cap) * item_size);
+//         if (ptr)
+//         {
+//             ARRAY_HEADER_CAST(items)->cap = new_cap;
+//             return items;
+//         }
+//         else // WARN(bcall): memory allocation fails hiddenly... 
+//         {
+//             return items; 
+//         }
+//     }
+//     else
+//     {
+//         // NOTE(bcall): push room for entire new cap and move to new location...
+//         void* ptr = arena_push(arena, sizeof(ArrayHeader) + new_cap * item_size);
+//         if (ptr)
+//         {
+//             ArrayHeader* hdr = (ArrayHeader*)ptr;
+//             hdr->cap = new_cap;
+//
+//             ptr = (void*)(hdr + 1);
+//             memmove(ptr, items, len * item_size);
+//
+//             // TODO(bcall): free prev items pointer if arena has free list...
+//             return ptr;
+//
+//         }
+//         else // WARN(bcall): memory allocation fails hiddenly... 
+//         {
+//             return items;
+//         }
+//     }
+// }
+//
+// /*********************************************************************************/
+// void* array_grow_heap(void* items, uint64_t item_size, uint64_t count)
+// {
+//     void* new_ptr;
+//     uint64_t new_len;
+//     uint64_t new_cap;
+//
+//     uint64_t len = ARRAY_LEN(items);
+//     uint64_t cap = ARRAY_CAP(items);
+//
+//     new_len = len + count;
+//     if (new_len < cap)
+//         return items;
+//
+//     if (new_len < 2 * cap)
+//         new_cap = 2 * cap;
+//     else if (new_len < 4)
+//         new_cap = 4;
+//     else
+//         new_cap = (uint64_t)(3 * new_len / 2);
+//
+//     if (items == NULL)
+//     {
+//         void* ptr = malloc(sizeof(ArrayHeader) + new_cap * item_size);
+//         if (ptr)
+//         {
+//             ArrayHeader* hdr = (ArrayHeader*)ptr;
+//             hdr->cap = new_cap;
+//             hdr->len = 0;
+//             return (void*)(hdr + 1);
+//         }
+//         else // WARN(bcall): memory allocation fails hiddenly... 
+//         {
+//             return items; 
+//         }
+//     }
+//     else
+//     {
+//         void* ptr = realloc(ARRAY_HEADER_CAST(items), sizeof(ArrayHeader) + new_cap * item_size);
+//         if (ptr)
+//         {
+//             ArrayHeader* hdr = (ArrayHeader*)ptr;
+//             hdr->cap = new_cap;
+//             hdr->len = len;
+//
+//             ptr = (void*)(hdr + 1);
+//             memmove(ptr, items, len * item_size);
+//
+//             return ptr;
+//
+//         }
+//         else // WARN(bcall): memory allocation fails hiddenly... 
+//         {
+//             return items;
+//         }
+//     }
+// }
 
 
--- base_ds.h
+++ base_ds.h
@@ -20,12 +20,9 @@
 // STACK
 //*******************
 
-/*
+// void  STACK_PUSH  (T* first, T* node)    Pushes node to front of stack
+// void  STACK_POP   (T* first)             Pops node from front of stack. Node is not returned.
 
-void  STACK_PUSH  (T* first, T* node)    Pushes node to front of stack
-void  STACK_POP   (T* first)          Pops node from front of stack. Node is not returned.
-
-*/
 
 #define STACK_PUSH(first, node)   STACK_PUSH_N(first, node, next)
 #define STACK_POP(first)          STACK_POP_N(first, next)
@@ -42,12 +39,8 @@ void  STACK_POP   (T* first)          Pops node from front of stack. Node is not
 // QUEUE
 //*******************
 
-/*
-
-void  QUEUE_PUSH  (T* first, T* last, T* node)    Pushes node to end of queue
-void  QUEUE_POP   (T* first, T* last)          Pops node from start of queue. Node is not returned.
-
-*/
+// void  QUEUE_PUSH  (T* first, T* last, T* node)    Pushes node to end of queue
+// void  QUEUE_POP   (T* first, T* last)             Pops node from start of queue. Node is not returned.
 
 #define QUEUE_PUSH(first, last, node)    QUEUE_PUSH_N(first, last, node, next)
 #define QUEUE_POP(first, last)           QUEUE_POP_N(first, last, next) 
@@ -67,19 +60,16 @@ void  QUEUE_POP   (T* first, T* last)          Pops node from start of queue. No
 // DOUBLY-LINKED LIST
 //*******************
 
-/*
+// void  DLL_PUSH_BACK      (T* first, T* last, T* node)                 Pushes node to end of list
+// void  DLL_PUSH_FRONT     (T* first, T* last, T* node)                 Pushes node to front of list
+//
+// void  DLL_INSERT_AFTER   (T* first, T* last, T* ref_node, T* node)    Inserts node after reference node in list
+// void  DLL_INSERT_BEFORE  (T* first, T* last, T* ref_node, T* node)    Inserts node before reference node in list
+//
+// void  DLL_REMOVE         (T* first, T* last, T* node)                 Removes node from list
+// void  DLL_REMOVE_FIRST   (T* first, T* last)                          Removes node from front of list
+// void  DLL_REMOVE_LAST    (T* first, T* last)                          Removes node from end of list
 
-void  DLL_PUSH_BACK      (T* first, T* last, T* node)                 Pushes node to end of list
-void  DLL_PUSH_FRONT     (T* first, T* last, T* node)                 Pushes node to front of list
-
-void  DLL_INSERT_AFTER   (T* first, T* last, T* ref_node, T* node)    Inserts node after reference node in list
-void  DLL_INSERT_BEFORE  (T* first, T* last, T* ref_node, T* node)    Inserts node before reference node in list
-
-void  DLL_REMOVE         (T* first, T* last, T* node)                 Removes node from list
-void  DLL_REMOVE_FIRST   (T* first, T* last)                          Removes node from front of list
-void  DLL_REMOVE_LAST    (T* first, T* last)                          Removes node from end of list
-
-*/
 
 #define DLL_PUSH_BACK(first, last, node)                  DLL_PUSH_BACK_NP(first, last, node, next, prev)
 #define DLL_PUSH_FRONT(first, last, node)                 DLL_PUSH_FRONT_NP(first, last, node, next, prev)
@@ -125,31 +115,28 @@ void  DLL_REMOVE_LAST    (T* first, T* last)                          Removes no
 // DYNAMIC ARRAY
 //*******************
 
-/*  
-   
-size_t  ARRAY_LEN          (T* array)                        Returns number of elements in array          
-size_t  ARRAY_CAP          (T* array)                        Returns number of elements array can hold before resizing
-T       ARRAY_LAST         (T* array)                        Returns last item of array as an lvalue
+// size_t  ARRAY_LEN          (T* array)                        Returns number of elements in array          
+// size_t  ARRAY_CAP          (T* array)                        Returns number of elements array can hold before resizing
+// T       ARRAY_LAST         (T* array)                        Returns last item of array as an lvalue
+//
+// void    ARRAY_SET_LEN      (T* array)                        Sets the length of the array. Leaves slots uninitialized
+// void    ARRAY_SET_CAP      (T* array)                        Sets the capacity of the array. Cannot shrink array
+//
+// T       ARRAY_PUSH         (T* array, T item)                Copies item to end of array. Returns item
+// void    ARRAY_POP          (T* array)                        Removes last item from array and returns the item
+//
+// T*      ARRAY_ADDN_PTR     (T* array, size_t n)              Adds n unitialized items to array and returns pointer to first unitialized item
+// size_t  ARRAY_ADDN_INDEX   (T* array, size_t n)              Adds n unitialized items to array and returns index to first unitialized item
+//
+// T       ARRAY_INSERT       (T* array, size_t i, T item)      Copies item into ith index of array and shifts items to make space. Returns item.
+// void    ARRAY_INSERT_N     (T* array, size_t i, size_t n)    Creates room for n uninitialized entries starting from index i
+//
+// void    ARRAY_DELETE       (T* array, size_t i)              Deletes ith item from array and shifts items after down
+// void    ARRAY_DELETE_N     (T* array, size_t i, size_t n)    Detetes n items starting from index i and shifts items after down  
+// void    ARRAY_DELETE_SWAP  (T* array, size_t i)              Deletes ith item from array and swaps last item instead of shifting down
+//
+// void    ARRAY_FREE         (T* array)                        Frees array memory and sets array pointer to NULL
 
-void    ARRAY_SET_LEN      (T* array)                        Sets the length of the array. Leaves slots uninitialized
-void    ARRAY_SET_CAP      (T* array)                        Sets the capacity of the array. Cannot shrink array
-
-T       ARRAY_PUSH         (T* array, T item)                Copies item to end of array. Returns item
-void    ARRAY_POP          (T* array)                        Removes last item from array and returns the item
-
-T*      ARRAY_ADDN_PTR     (T* array, size_t n)              Adds n unitialized items to array and returns pointer to first unitialized item
-size_t  ARRAY_ADDN_INDEX   (T* array, size_t n)              Adds n unitialized items to array and returns index to first unitialized item
-
-T       ARRAY_INSERT       (T* array, size_t i, T item)      Copies item into ith index of array and shifts items to make space. Returns item.
-void    ARRAY_INSERT_N     (T* array, size_t i, size_t n)    Creates room for n uninitialized entries starting from index i
-
-void    ARRAY_DELETE       (T* array, size_t i)              Deletes ith item from array and shifts items after down
-void    ARRAY_DELETE_N     (T* array, size_t i, size_t n)    Detetes n items starting from index i and shifts items after down  
-void    ARRAY_DELETE_SWAP  (T* array, size_t i)              Deletes ith item from array and swaps last item instead of shifting down
-
-void    ARRAY_FREE         (T* array)                        Frees array memory and sets array pointer to NULL
-
-*/
 
 #define ARRAY_LEN          stbds_arrlenu
 #define ARRAY_CAP          stbds_arrcap
@@ -171,32 +158,29 @@ void    ARRAY_FREE         (T* array)                        Frees array memory 
 // HASH MAP
 //************************
 
-/*
+// size_t     HMAP_LEN           (T* map)                           Returns number of items in map. Can be used to iterate over items in map array.
+//
+// void       HMAP_DEFAULT       (T* map, TV value)                 Sets the default value returned by HM_GET if key is not present
+// void       HMAP_DEFAULTS      (T* map, T struct)                 Sets the default struct returned by HM_GETS if key is not present
+//
+// void       HMAP_PUT           (T* map, TK key, TV value)         Update existing key, value or add new item if key is new         
+// void       HMAP_PUTS          (T* map, T struct)                 Same as HMAP_PUT but takes in struct with key and value fields
+//
+// TV         HMAP_GET           (T* map, TK key)                   Returns the value of the corresponding key, or default if key is missing
+// TV         HMAP_GET_TS        (T* map, TK key, ptrdiff_t tmp)    Same as HMAP_GET but thread safe
+// T          HMAP_GETS          (T* map, TK key)                   Returns the struct of the corresponding key, or default if key is missing
+//
+// T*         HMAP_GET_PTR       (T* map, TK key)                   Returns pointer to struct corresponding to key or pointer to default if key is missing
+// T*         HMAP_GET_PTR_TS    (T* map, TK key, ptrdiff_t tmp)    Same at HMAP_GET_PTR but thread safe
+// T*         HMAP_GET_PTR_NULL  (T* map, TK key)                   Same as HMAP_GET_PTR but retunrs null if key is missing
+//
+// ptrdiff_t  HMAP_GET_IDX       (T* map, TK key)                   Returns the index of key into map array or -1 if key is missing
+// ptrdiff_t  HMAP_GET_IDX_TS    (T* map, ptrdiff_t tmp)            Same as HMAP_GET_IDX but thread safe
+//
+// int        HMAP_DEL           (T* map, TK key)                   Deletes item corresponding to key from hashmap and returns 1. Returns 0 otherwise.
+//
+// void       HMAP_FREE          (T* map)                           Frees hash map memory and sets pointer to NULL
 
-size_t     HMAP_LEN           (T* map)                           Returns number of items in map. Can be used to iterate over items in map array.
-
-void       HMAP_DEFAULT       (T* map, TV value)                 Sets the default value returned by HM_GET if key is not present
-void       HMAP_DEFAULTS      (T* map, T struct)                 Sets the default struct returned by HM_GETS if key is not present
-
-void       HMAP_PUT           (T* map, TK key, TV value)         Update existing key, value or add new item if key is new         
-void       HMAP_PUTS          (T* map, T struct)                 Same as HMAP_PUT but takes in struct with key and value fields
-
-TV         HMAP_GET           (T* map, TK key)                   Returns the value of the corresponding key, or default if key is missing
-TV         HMAP_GET_TS        (T* map, TK key, ptrdiff_t tmp)    Same as HMAP_GET but thread safe
-T          HMAP_GETS          (T* map, TK key)                   Returns the struct of the corresponding key, or default if key is missing
-
-T*         HMAP_GET_PTR       (T* map, TK key)                   Returns pointer to struct corresponding to key or pointer to default if key is missing
-T*         HMAP_GET_PTR_TS    (T* map, TK key, ptrdiff_t tmp)    Same at HMAP_GETP but thread safe
-T*         HMAP_GET_PTR_NULL  (T* map, TK key)                   Same as HMAP_GETP but retunrs null if key is missing
-
-ptrdiff_t  HMAP_GET_IDX       (T* map, TK key)                   Returns the index of key into map array or -1 if key is missing
-ptrdiff_t  HMAP_GET_IDX_TS    (T* map, ptrdiff_t tmp)            Same as HMAP_GETI but thread safe
-
-int        HMAP_DEL           (T* map, TK key)                   Deletes item corresponding to key from hashmap and returns 1. Returns 0 otherwise.
-
-void       HMAP_FREE          (T* map)                           Frees hash map memory and sets pointer to NULL
-
-*/
 
 #define HMAP_LEN           stbds_hmlenu
 #define HMAP_DEFAULT       stbds_hmdefault
@@ -218,26 +202,47 @@ void       HMAP_FREE          (T* map)                           Frees hash map 
 // STRING HASH MAP
 //************************
 
-// #define SMAP_PUT         stbds_shput
-// #define SMAP_PUTI        stbds_shputi
-// #define SMAP_PUTS        stbds_shputs
-// #define SMAP_GET         stbds_shget
-// #define SMAP_GETI        stbds_shgeti
-// #define SMAP_GETS        stbds_shgets
-// #define SMAP_GETP        stbds_shgetp
-// #define SMAP_GETP_NULL   stbds_shgetp_null
-// #define SMAP_DEL         stbds_shdel
-// #define SMAP_LEN         stbds_shlen
-// #define SMAP_LENU        stbds_shlenu
-// #define SMAP_FREE        stbds_shfree
-// #define SMAP_DEFAULT     stbds_shdefault
-// #define SMAP_DEFAULTS    stbds_shdefaults
-// #define SMAP_NEW_ARENA   stbds_sh_new_arena
-// #define SMAP_NEW_STRDUP  stbds_sh_new_strdup
+// size_t     SMAP_LEN           (T* map)                         Returns number of items in map. Can be used to iterate over items in map array.
 //
-// #define SMAP_STR_ALLOC   stbds_stralloc
-// #define SMAP_STR_RESET   stbds_strreset
+// void       SMAP_DEFAULT       (T* map, TV value)               Sets the default value returned by SMAP_GET if key is not present
+// void       SMAP_DEFAULTS      (T* map, T struct)               Sets the default struct returned by SMAP_GETS if key is not present
+//
+// void       SMAP_PUT           (T* map, char* key, TV value)    Update existing key, value or add new item if key is new         
+// void       SMAP_PUTI          (T* map, char* key, TV value)    Same as SMAP_PUT but returns index of item into item array
+// void       SMAP_PUTS          (T* map, T struct)               Same as SMAP_PUT but takes in struct with key and value fields
+//
+// TV         SMAP_GET           (T* map, char* key)              Returns the value of the corresponding key, or default if key is missing
+// T          SMAP_GETS          (T* map, char* key)              Returns the struct of the corresponding key, or default if key is missing
+//
+// T*         SMAP_GET_PTR       (T* map, char* key)              Returns pointer to struct corresponding to key or pointer to default if key is missing
+// T*         SMAP_GET_PTR_NULL  (T* map, char* key)              Same as SMAP_GET_PTR but retunrs null if key is missing
+//
+// ptrdiff_t  SMAP_GET_IDX       (T* map, char* key)              Returns the index of key into map array or -1 if key is missing
+//
+// int        SMAP_DEL           (T* map, char* key)              Deletes item corresponding to key from hashmap and returns 1. Returns 0 otherwise.
+//
+// void       SMAP_FREE          (T* map)                         Frees hash map memory and sets pointer to NULL
 
+
+#define SMAP_NEW_ARENA     stbds_sh_new_arena
+#define SMAP_NEW_STRDUP    stbds_sh_new_strdup
+
+#define SMAP_LEN           stbds_shlenu
+#define SMAP_DEFAULT       stbds_shdefault
+#define SMAP_DEFAULTS      stbds_shdefaults
+#define SMAP_PUT           stbds_shput
+#define SMAP_PUTS          stbds_shputs
+#define SMAP_PUT_IDX       stbds_shputi
+#define SMAP_GET           stbds_shget
+#define SMAP_GETS          stbds_shgets
+#define SMAP_GET_PTR       stbds_shgetp
+#define SMAP_GET_PTR_NULL  stbds_shgetp_null
+#define SMAP_GET_IDX       stbds_shgeti
+#define SMAP_DEL           stbds_shdel
+#define SMAP_FREE          stbds_shfree
+
+#define SMAP_STR_ALLOC     stbds_stralloc
+#define SMAP_STR_RESET     stbds_strreset
 
 #endif // BASE_DATA_STRUCTURES_H
       
