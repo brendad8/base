781fdf31cf039f47bc191b548318769c7cc1b05d
Brendan Callender <bcall@ssreng.com>
2026-06-18 15:58:56 -0700

    no debug info for test cases by default

--- test/test.bat
+++ test/test.bat
@@ -1,8 +1,8 @@
 @echo off
 
-zig cc arena_test.c -o arena_test.exe
-zig cc ds_test.c -o ds_test.exe
-zig cc string_test.c -o string_test.exe
+zig cc arena_test.c -o arena_test.exe -s
+zig cc ds_test.c -o ds_test.exe -s
+zig cc string_test.c -o string_test.exe -s
 
 arena_test.exe
 ds_test.exe
