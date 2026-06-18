1289b7cf437fdb7b2638a9a6fc80bddd910c6c96
Brendan Callender <bcall@ssreng.com>
2026-06-18 09:36:37 -0700

    initial migration to stb_ds data structures

--- base_ds.h
+++ base_ds.h
@@ -9,6 +9,9 @@
 #include "base_core.h"
 #include "base_arena.h"
 
+#define STBDS_NO_SHORT_NAMES
+#include "deps/stb_ds.h"
+
 //***************************************************************************
 //          MACROS
 //***************************************************************************
@@ -17,20 +20,34 @@
 // STACK
 //*******************
 
+/*
+
+void  STACK_PUSH  (T* first, T* node)    Pushes node to front of stack
+void  STACK_POP   (T* first)          Pops node from front of stack. Node is not returned.
+
+*/
+
+#define STACK_PUSH(first, node)   STACK_PUSH_N(first, node, next)
+#define STACK_POP(first)          STACK_POP_N(first, next)
+
 #define STACK_PUSH_N(first, node, next) \
     ((node)->next = (first), (first) = (node))
 
 #define STACK_POP_N(first, next) \
     ((first) = (first)->next)
 
-#define STACK_PUSH(first, node) STACK_PUSH_N(first, node, next)
-
-#define STACK_POP(first) STACK_POP_N(first, next)
 
 
 //*******************
 // QUEUE
 //*******************
+
+/*
+
+void  QUEUE_PUSH  (T* first, T* last, T* node)    Pushes node to end of queue
+void  QUEUE_POP   (T* first, T* last)          Pops node from start of queue. Node is not returned.
+
+*/
 
 #define QUEUE_PUSH(first, last, node)    QUEUE_PUSH_N(first, last, node, next)
 #define QUEUE_POP(first, last)           QUEUE_POP_N(first, last, next) 
@@ -49,6 +66,20 @@
 //*******************
 // DOUBLY-LINKED LIST
 //*******************
+
+/*
+
+void  DLL_PUSH_BACK      (T* first, T* last, T* node)                 Pushes node to end of list
+void  DLL_PUSH_FRONT     (T* first, T* last, T* node)                 Pushes node to front of list
+
+void  DLL_INSERT_AFTER   (T* first, T* last, T* ref_node, T* node)    Inserts node after reference node in list
+void  DLL_INSERT_BEFORE  (T* first, T* last, T* ref_node, T* node)    Inserts node before reference node in list
+
+void  DLL_REMOVE         (T* first, T* last, T* node)                 Removes node from list
+void  DLL_REMOVE_FIRST   (T* first, T* last)                          Removes node from front of list
+void  DLL_REMOVE_LAST    (T* first, T* last)                          Removes node from end of list
+
+*/
 
 #define DLL_PUSH_BACK(first, last, node)                  DLL_PUSH_BACK_NP(first, last, node, next, prev)
 #define DLL_PUSH_FRONT(first, last, node)                 DLL_PUSH_FRONT_NP(first, last, node, next, prev)
@@ -94,156 +125,119 @@
 // DYNAMIC ARRAY
 //*******************
 
-typedef struct ArrayHeader ArrayHeader;
-struct ArrayHeader
-{
-    uint64_t len;
-    uint64_t cap;
-};
+/*  
+   
+size_t  ARRAY_LEN          (T* array)                        Returns number of elements in array          
+size_t  ARRAY_CAP          (T* array)                        Returns number of elements array can hold before resizing
+T       ARRAY_LAST         (T* array)                        Returns last item of array as an lvalue
 
-#define ARRAY_HEADER_CAST(a)                  (((ArrayHeader*)(a))-1)
-#define ARRAY_ITEM_SIZE(a)                    (sizeof(*(a)))
-#define ARRAY_LEN(a)                          ((a) ? ARRAY_HEADER_CAST((a))->len : 0)
-#define ARRAY_CAP(a)                          ((a) ? ARRAY_HEADER_CAST((a))->cap : 0)
+void    ARRAY_SET_LEN      (T* array)                        Sets the length of the array. Leaves slots uninitialized
+void    ARRAY_SET_CAP      (T* array)                        Sets the capacity of the array. Cannot shrink array
 
-#define ARRAY_REMOVE(a, i)                    (ARRAY_REMOVEN((a), (i), 1))
-#define ARRAY_REMOVEN(a, i, n)                ((i) + (n) < ARRAY_LEN(a) ? memmove(&(a)[i], &(a)[(i)+(n)], ARRAY_ITEM_SIZE(a) * (ARRAY_LEN(a)-(n)-(i))), ARRAY_HEADER_CAST(a)->len -= (n) : 0)
-#define ARRAY_REMOVE_SWAP(a,i)                ((i) < ARRAY_LEN(a) ? (a)[i] = (a)[ARRAY_LEN(a)-1], ARRAY_HEADER_CAST(a)->len-- : 0)
+T       ARRAY_PUSH         (T* array, T item)                Copies item to end of array. Returns item
+void    ARRAY_POP          (T* array)                        Removes last item from array and returns the item
 
-#define ARRAY_CLEAR(a)                        (ARRAY_HEADER_CAST(a)->len = 0)
-#define ARRAY_CLEAR_ZERO(a)                   (memset((a), 0, ARRAY_ITEM_SIZE(a)*ARRAY_LEN(a)), ARRAY_HEADER_CAST(a)->len = 0)
+T*      ARRAY_ADDN_PTR     (T* array, size_t n)              Adds n unitialized items to array and returns pointer to first unitialized item
+size_t  ARRAY_ADDN_INDEX   (T* array, size_t n)              Adds n unitialized items to array and returns index to first unitialized item
 
-//*******************
-// HEAP BACKED ARRAY
-//*******************
+T       ARRAY_INSERT       (T* array, size_t i, T item)      Copies item into ith index of array and shifts items to make space. Returns item.
+void    ARRAY_INSERT_N     (T* array, size_t i, size_t n)    Creates room for n uninitialized entries starting from index i
 
-#define ARRAY_RESERVE(a, n)                   ((a) = array_grow_heap((a), ARRAY_ITEM_SIZE(a), (n)))
-// #define ARRAY_SETCAP(a, cap)                   void
+void    ARRAY_DELETE       (T* array, size_t i)              Deletes ith item from array and shifts items after down
+void    ARRAY_DELETE_N     (T* array, size_t i, size_t n)    Detetes n items starting from index i and shifts items after down  
+void    ARRAY_DELETE_SWAP  (T* array, size_t i)              Deletes ith item from array and swaps last item instead of shifting down
 
-#define ARRAY_PUSH(a, item)                   ((a) = array_grow_heap((a), ARRAY_ITEM_SIZE(a), 1), (a)[ARRAY_HEADER_CAST(a)->len++] = (item))
-#define ARRAY_ADD(a)                          (ARRAY_ADDN((a), 1))
-#define ARRAY_ADDN(a, n)                      ((a) = array_grow_heap((a), ARRAY_ITEM_SIZE(a), (n)), ARRAY_HEADER_CAST(a)->len += (n), &(a)[ARRAY_LEN(a) - (n)])
+void    ARRAY_FREE         (T* array)                        Frees array memory and sets array pointer to NULL
 
-#define ARRAY_INSERTN(a, i, n)                (ARRAY_ADDN(a, n), memmove(&(a)[(i)+(n)], &(a)[i], ARRAY_ITEM_SIZE(a) * (ARRAY_LEN(a) - (n) - (i))))
-#define ARRAY_INSERT(a, i, item)              (ARRAY_INSERTN(a, i, 1), (a)[i] = item)
+*/
 
-#define ARRAY_FREE(a)                         ((a) ? free(ARRAY_HEADER_CAST(a)), 1 : 0)
-
-//*******************
-// ARENA BACKED ARRAY
-//*******************
-
-// TODO(bcall): reserve n additional or reserve cap of n...
-#define ARRAY_RESERVE_ARENA(arena, a, n)      ((a) = array_grow_arena((arena), (a), ARRAY_ITEM_SIZE(a), (n)))
-// #define ARRAY_SETCAP_ARENA(arena, a, cap)     void     
-
-#define ARRAY_PUSH_ARENA(arena, a, item)      ((a) = array_grow_arena((arena), (a), ARRAY_ITEM_SIZE(a), 1), (a)[ARRAY_HEADER_CAST(a)->len++] = (item))
-#define ARRAY_ADD_ARENA(arena, a)             ARRAY_ADDN_ARENA((arena), (a), 1) 
-#define ARRAY_ADDN_ARENA(arena, a, n)         (((a) = array_grow_arena((arena), (a), ARRAY_ITEM_SIZE(a), (n))), ARRAY_HEADER_CAST(a)->len += (n), &(a)[ARRAY_LEN(a) - (n)])
-
-#define ARRAY_INSERTN_ARENA(arena, a, i, n)   (ARRAY_ADDN_ARENA(arena, a, n), memmove(&(a)[(i)+(n)], &(a)[i], ARRAY_ITEM_SIZE(a) * (ARRAY_LEN(a) - (n) - (i))))
-#define ARRAY_INSERT_ARENA(arena, a, i, item) (ARRAY_INSERTN_ARENA(arena, a, i, 1), (a)[i] = item)
+#define ARRAY_LEN          stbds_arrlenu
+#define ARRAY_CAP          stbds_arrcap
+#define ARRAY_LAST         stbds_arrlast
+#define ARRAY_SET_LEN      stbds_arrsetlen
+#define ARRAY_SET_CAP      stbds_arrsetcap
+#define ARRAY_PUSH         stbds_arrput
+#define ARRAY_POP          stbds_arrpop
+#define ARRAY_ADDN_PTR     stbds_arraddnptr
+#define ARRAY_ADDN_INDEX   stbds_arraddnindex
+#define ARRAY_INSERT       stbds_arrins
+#define ARRAY_INSERT_N     stbds_arrinsn
+#define ARRAY_DELETE       stbds_arrdel
+#define ARRAY_DELETE_N     stbds_arrdeln
+#define ARRAY_DELETE_SWAP  stbds_arrdelswap
+#define ARRAY_FREE         stbds_arrfree
 
 //************************
-// Hash Map
+// HASH MAP
 //************************
 
-static const float map_grow_threshold      = 0.75f; // 12/16
-static const float map_tombstone_threshold = 0.19f; //  3/16
-static const float map_shrink_threshold    = 0.25f; //  4/16
+/*
 
-typedef enum
-{
-    MAP_ENTRY_FREE  = 0,
-    MAP_ENTRY_TOMB  = 1,
-    MAP_ENTRY_TAKEN = 2
+size_t     HMAP_LEN           (T* map)                           Returns number of items in map. Can be used to iterate over items in map array.
 
-} MapEntryState;
+void       HMAP_DEFAULT       (T* map, TV value)                 Sets the default value returned by HM_GET if key is not present
+void       HMAP_DEFAULTS      (T* map, T struct)                 Sets the default struct returned by HM_GETS if key is not present
 
-typedef struct MapEntry MapEntry;
-struct MapEntry
-{
-    size_t   hash;
-    uint32_t idx;
-    MapEntryState state;
-};
+void       HMAP_PUT           (T* map, TK key, TV value)         Update existing key, value or add new item if key is new         
+void       HMAP_PUTS          (T* map, T struct)                 Same as HMAP_PUT but takes in struct with key and value fields
 
-typedef struct MapHeader MapHeader;
-struct MapHeader
-{
-    size_t len;       // hashmap len
-    size_t cap;       // hashmap capacity 
-    size_t ts_count;  // tombstone count
-    size_t offset;    // 
-};
+TV         HMAP_GET           (T* map, TK key)                   Returns the value of the corresponding key, or default if key is missing
+TV         HMAP_GET_TS        (T* map, TK key, ptrdiff_t tmp)    Same as HMAP_GET but thread safe
+T          HMAP_GETS          (T* map, TK key)                   Returns the struct of the corresponding key, or default if key is missing
 
-#define HMAP_HEADER_CAST(m)   ((MapHeader *)((uint8_t *)(m) - (HMAP_ITEM_SIZE(m)) - sizeof(MapHeader)))
-#define HMAP_LEN(m)           ((m) ? HMAP_HEADER_CAST((m))->len : 0) 
-#define HMAP_CAP(m)           ((m) ? HMAP_HEADER_CAST((m))->cap : 0) 
-#define HMAP_ITEM_SIZE(m)     (sizeof(*(m)))
-#define HMAP_KEY_SIZE(m)      (sizeof((m)->key))
-#define HMAP_ENTRIES_CAST(m)  ((MapEntry*)((m)+(HMAP_ITEM_SIZE((m))*HMAP_CAP((m)))))
+T*         HMAP_GET_PTR       (T* map, TK key)                   Returns pointer to struct corresponding to key or pointer to default if key is missing
+T*         HMAP_GET_PTR_TS    (T* map, TK key, ptrdiff_t tmp)    Same at HMAP_GETP but thread safe
+T*         HMAP_GET_PTR_NULL  (T* map, TK key)                   Same as HMAP_GETP but retunrs null if key is missing
 
-// #define HMAP_HEADER_CAST(m)   (((MapHeader*)((m)-1))-1)
+ptrdiff_t  HMAP_GET_IDX       (T* map, TK key)                   Returns the index of key into map array or -1 if key is missing
+ptrdiff_t  HMAP_GET_IDX_TS    (T* map, ptrdiff_t tmp)            Same as HMAP_GETI but thread safe
 
-#define HMAP_SEED 822
+int        HMAP_DEL           (T* map, TK key)                   Deletes item corresponding to key from hashmap and returns 1. Returns 0 otherwise.
 
-// #define HMAP_RESERVE(m, n)
-// #define HMAP_DEFAULT(m, val)
-// #define HMAP_CLEAR(m)
+void       HMAP_FREE          (T* map)                           Frees hash map memory and sets pointer to NULL
 
-#define HMAP_PUT(m, k, val) \
-    do { \
-        (m) = hmap_grow_heap((m), HMAP_ITEM_SIZE(m), HMAP_KEY_SIZE(m), 1); \
-        size_t _idx = hmap_insert((m), &(k), HMAP_KEY_SIZE(m), HMAP_SEED); \
-        m[_idx].key   = k; \
-        m[_idx].value = val; \
-    } while(0)
+*/
 
-// #define HMAP_RESERVE(arena, m, n)
-// #define HMAP_DEFAULT(arena, m, val)
-// #define HMAP_DEFAULTS(arena, m, s)
-// #define HMAP_CLEAR(m)
-// #define HMAP_CLONE(m)
-
-// #define HMAP_PUT(arena, m, k, val)
-// #define HMAP_INDEX(arena, m, k)
-// #define HMAP_GET(m, type, k)
-// #define HMAP_TRY_GET(m, k, out_val)
-// #define HMAP_GETS(m, type, k)
-// #define HMAP_GET_PTR(m, k)
-// #define HMAP_GET_PTR_NULL(m, k)
-// #define HMAP_DEL(scratch, m, k)
+#define HMAP_LEN           stbds_hmlenu
+#define HMAP_DEFAULT       stbds_hmdefault
+#define HMAP_DEFAULTS      stbds_hmdefaults
+#define HMAP_PUT           stbds_hmput
+#define HMAP_PUTS          stbds_hmputs
+#define HMAP_GET           stbds_hmget
+#define HMAP_GET_TS        stbds_hmget_ts
+#define HMAP_GETS          stbds_hmgets
+#define HMAP_GET_PTR       stbds_hmgetp
+#define HMAP_GET_PTR_TS    stbds_hmgetp_ts
+#define HMAP_GET_PTR_NULL  stbds_hmgetp_null
+#define HMAP_GET_IDX       stbds_hmgeti
+#define HMAP_GET_IDX_TS    stbds_hmgeti_ts
+#define HMAP_DEL           stbds_hmdel
+#define HMAP_FREE          stbds_hmfree
 
 //************************
-// String Hash Map
+// STRING HASH MAP
 //************************
 
-// #define SMAP_RESERVE(arena, m, n)
-// #define SMAP_DEFAULT(arena, m, val)
-// #define SMAP_DEFAULTS(arena, m, s)
-// #define SMAP_CLEAR(m)
-// #define SMAP_CLONE(m)
+// #define SMAP_PUT         stbds_shput
+// #define SMAP_PUTI        stbds_shputi
+// #define SMAP_PUTS        stbds_shputs
+// #define SMAP_GET         stbds_shget
+// #define SMAP_GETI        stbds_shgeti
+// #define SMAP_GETS        stbds_shgets
+// #define SMAP_GETP        stbds_shgetp
+// #define SMAP_GETP_NULL   stbds_shgetp_null
+// #define SMAP_DEL         stbds_shdel
+// #define SMAP_LEN         stbds_shlen
+// #define SMAP_LENU        stbds_shlenu
+// #define SMAP_FREE        stbds_shfree
+// #define SMAP_DEFAULT     stbds_shdefault
+// #define SMAP_DEFAULTS    stbds_shdefaults
+// #define SMAP_NEW_ARENA   stbds_sh_new_arena
+// #define SMAP_NEW_STRDUP  stbds_sh_new_strdup
+//
+// #define SMAP_STR_ALLOC   stbds_stralloc
+// #define SMAP_STR_RESET   stbds_strreset
 
-// #define SMAP_PUT(arena, m, k, val)
-// #define SMAP_INDEX(arena, m, k)
-// #define SMAP_GET(m, type, k)
-// #define SMAP_TRY_GET(m, k, out_val)
-// #define SMAP_GETS(m, type, k)
-// #define SMAP_GET_PTR(m, k)
-// #define SMAP_GET_PTR_NULL(m, k)
-// #define SMAP_DEL(scratch, m, k)
-
-//***************************************************************************
-//          FUNCTION PROTOTYPES
-//***************************************************************************
-
-void*   array_grow_arena     (Arena* arena, void* items, uint64_t item_size, uint64_t count);
-void*   array_grow_heap      (void* items, uint64_t item_size, uint64_t count);
-
-size_t  hmap_insert          (void* map, void* key_ptr, size_t key_size, int32_t seed);
-void    hmap_rehash_entries  (void* map, void* new_map, size_t new_cap, size_t item_size, size_t key_size);
-void*   hmap_grow_heap       (void* map, size_t item_size, size_t key_size, size_t count);
 
 #endif // BASE_DATA_STRUCTURES_H
       
