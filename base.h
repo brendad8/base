9eec70f891155b43a391936c6391ba4ff398a402
Brendan Callender <72055001+brendad8@users.noreply.github.com>
2026-08-26 19:08:26 -0700

    def -> base

---
+++ base.h
@@ -0,0 +1,123 @@
+
+#ifndef BASE_H
+#define BASE_H
+
+/***************************************************************************
+ *          INCLUDES
+ ***************************************************************************/
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/***************************************************************************
+ *          MACROS
+ ***************************************************************************/
+
+#define KB(n)  (((uint64_t)(n)) << 10)
+#define MB(n)  (((uint64_t)(n)) << 20)
+#define GB(n)  (((uint64_t)(n)) << 30)
+#define TB(n)  (((uint64_t)(n)) << 40)
+
+#define THOUSAND(n) ((n)*1000)
+#define MILLION(n)  ((n)*1000000)
+#define BILLION(n)  ((n)*1000000000)
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+#define CLAMP_TOP(val,max) MIN(val,max)
+#define CLAMP_BOT(val,min) MAX(val,min)
+#define CLAMP(min,val,max) (((val)<(min))?(min):((val)>(max))?(max):(val))
+
+#define ARRAY_COUNT(a) (sizeof(a) / sizeof((a)[0]))
+
+#define CEIL_INTEGER_DIV(a,b) (((a) + (b) - 1)/(b))
+
+#define SWAP(T,a,b) do{T t__ = a; a = b; b = t__;}while(0)
+
+#define COMPOSE_64BIT(a,b)  ((((uint64)a) << 32) | ((uint64)b))
+#define COMPOSE_32BIT(a,b)  ((((uint32)a) << 16) | ((uint32)b))
+
+#define ALIGN_UP_POW2(x,b)   (((x) + (b) - 1)&(~((b) - 1)))
+#define ALIGN_DOWN_POW2(x,b) ((x)&(~((b) - 1)))
+
+#define IS_POW2(x)           ((x)!=0 && ((x)&((x)-1))==0)
+#define IS_POW2_OR_ZERO(x)   ((((x) - 1)&(x)) == 0)
+
+#define EXTRACT_BIT(word, idx) (((word) >> (idx)) & 1)
+#define EXTRACT8(word, pos)    (((word) >> ((pos)*8))  & max_uint8)
+#define EXTRACT16(word, pos)   (((word) >> ((pos)*16)) & max_uint16)
+#define EXTRACT32(word, pos)   (((word) >> ((pos)*32)) & max_uint32)
+
+/***************************************************************************
+ *          COMPILER
+ ***************************************************************************/
+
+#if defined(_MSC_VER)
+    #define COMPILER_MSVC 1
+#elif defined(__clang__)
+    #define COMPILER_CLANG 1
+#elif defined(__GNUC__)
+    #define COMPILER_GCC 1
+#else
+    #error Unknown compiler
+#endif
+
+
+/***************************************************************************
+ *          OPERATING SYSTEM
+ ***************************************************************************/
+
+#if defined(_WIN32)
+    #define OS_WINDOWS 1
+#elif defined(__linux__)
+    #define OS_LINUX 1
+#elif defined(__APPLE__)
+    #define OS_MACOS 1
+#else
+    #error Unknown platform
+#endif
+
+/***************************************************************************
+ *          ALIGNMENT
+ ***************************************************************************/
+
+#if COMPILER_MSVC
+    #define align_of(T) __alignof(T)
+#elif COMPILER_CLANG
+    #define align_of(T) __alignof(T)
+#elif COMPILER_GCC
+    #define align_of(T) __alignof__(T)
+#else
+    #error align_of not defined for this compiler.
+#endif
+
+#if COMPILER_MSVC
+    #define align_type(x) __declspec(align(x))
+#elif COMPILER_CLANG || COMPILER_GCC
+    #define align_type(x) __attribute__((aligned(x)))
+#else
+    #error align_type not defined for this compiler.
+#endif
+
+/***************************************************************************
+ *          ASSERT
+ ***************************************************************************/
+
+#if COMPILER_CLANG || COMPILER_GCC
+    #define trap() __builtin_trap()
+#elif COMPILER_MSVC
+    # define trap() __debugbreak()
+#else
+    #error Unknown trap intrinsic for this compiler.
+#endif
+
+#define assert_always(x) do{if(!(x)) {trap();}}while(0)
+
+#if BUILD_DEBUG
+    #define assert(x) assert_always(x)
+#else
+    #define assert(x) (void)(x)
+#endif
+
+#endif // BASE_H
